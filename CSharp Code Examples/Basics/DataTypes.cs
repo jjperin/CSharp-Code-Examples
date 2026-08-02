be51fdf94a699c61fc1e7d240a06a36e6b6c8736
jake peringatt <jakeperingatt@outlook.com>
2026-08-02 03:03:53 +0100

    Completed Datatypes.cs, containing data types, converting and casting

--- CSharp Code Examples/Basics/DataTypes.cs
+++ CSharp Code Examples/Basics/DataTypes.cs
@@ -8,10 +8,32 @@ namespace CSharp_Code_Examples
 {
     class DataTypes
     {
-        //Authors: Mark Doughty
+        //Authors: Mark Doughty, Jake Peringatt
         //Some data type uses in C#
         public static void run()
         {
+            //basic variables
+            int int_ = 5;
+            double double_ = 12.5;
+            string string_ = "abc";
+            char char_ = 'A';
+            bool bool_ = false;
+            Console.WriteLine($"int: {int_}, double: {double_}, string: {string_}, char: {char_}, bool: {bool_}");
+
+
+            //implicit casting 
+            double implicitCasting = int_;
+            Console.WriteLine("Int implicitly casteded to a Double: " + implicitCasting);
+
+            //explicit casting
+            int explicitCast = (int)double_;
+            Console.WriteLine("Double explicitly casted to an Int " + explicitCast);
+
+            //converting types
+
+            string numberString = "123";
+            int convertInt = Convert.ToInt32(numberString);
+            Console.WriteLine("String converted to an Int: " + convertInt);
 
         }
     }
