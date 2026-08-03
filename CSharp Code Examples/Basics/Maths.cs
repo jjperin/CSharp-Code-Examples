5d1596b77452b29f7e4e77b1d1cea0484f000412
jake peringatt <jakeperingatt@outlook.com>
2026-08-03 16:16:14 +0100

    Changed Maths.cs to for unit testing prep

--- CSharp Code Examples/Basics/Maths.cs
+++ CSharp Code Examples/Basics/Maths.cs
@@ -50,10 +50,16 @@ namespace CSharp_Code_Examples
 
             double a = Math.Abs(ValidDouble("Enter side a: "));
             double b = Math.Abs(ValidDouble("Enter side b: "));
-            double Pythag_squared = Math.Pow(a, 2) + Math.Pow(b, 2);
-            Console.WriteLine("Hypoteneuse = " + Math.Sqrt(Pythag_squared));
+            double c = CalculateHypotenuse(a, b);
+            Console.WriteLine("Hypoteneuse = " + c);
 
 
+        }
+
+        public static double CalculateHypotenuse(double a, double b)
+        {
+            double cSquared = Math.Pow(a, 2) + Math.Pow(b, 2);
+            return Math.Sqrt(cSquared);
         }
 
         public static void LawOfCosines()
@@ -77,10 +83,18 @@ namespace CSharp_Code_Examples
                     Console.WriteLine("Please input a valid angle for C, less than 180 degrees and more than 0");
                 }
             }
+
+
+            double unknownSide = CalculateLawOfCosines(a, b, C);
+            Console.WriteLine("Unknown side is = " + unknownSide);
+
+        }
+
+        public static double CalculateLawOfCosines(double a, double b, double C)
+        {
             double cRad = C * Math.PI / 180;
             double LoC_squared = Math.Pow(a, 2) + Math.Pow(b, 2) - (2 * a * b * Math.Cos(cRad));
-            Console.WriteLine("Unknown side is = " + Math.Round(Math.Sqrt(LoC_squared), 2));
-
+            return Math.Sqrt(LoC_squared);
         }
 
         public static void Circles()
@@ -88,9 +102,14 @@ namespace CSharp_Code_Examples
             //Circles
             //PI * R^2
             double radius = Math.Abs(ValidDouble("Please enter a number for the radius for the circle"));
-            double area = Math.PI * Math.Pow(radius, 2);
+            double area = CalculateArea(radius);
             Console.WriteLine("The area of the circle is = " + Math.Round(area, 2));
 
+        }
+
+        public static double CalculateArea(double radius)
+        {
+            return Math.PI* Math.Pow(radius, 2);
         }
 
         public static void QuadraticFormula()
@@ -104,32 +123,46 @@ namespace CSharp_Code_Examples
             double quadB = ValidDouble("Enter the 'b' coefficient: ");
             double quadC = ValidDouble("Enter the 'c' coefficient: ");
 
-            //Discriminant = b^2 - 4ac
-            double discriminant = Math.Pow(quadB, 2) - (4 * quadA * quadC);
+            double discriminant = CalculateDiscrimant(quadA,quadB,quadC);
 
 
             if (Math.Abs(quadA) < 0.000001) //floating point values can be inaccurate, so comparing directly to 0 may fail. this is to mitigate it.
             {
                 Console.WriteLine("Not a quadratic");
             }
-            else if (Math.Abs(discriminant) < 0.000001)
+            else if(discriminant < -0.000001)
             {
-                double root1 = (-quadB + Math.Sqrt(discriminant)) / (2 * quadA);
-                Console.WriteLine("One quadratic root: " + root1);
+                Console.WriteLine("No real quadratic roots exist");
             }
-            else if (discriminant > 0.000001)
+            else if(Math.Abs(discriminant) < 0.000001)
             {
-                double root1 = (-quadB + Math.Sqrt(discriminant)) / (2 * quadA);
-                double root2 = (-quadB - Math.Sqrt(discriminant)) / (2 * quadA);
-                Console.WriteLine("Two quadratic roots: " + root1 + ", " + root2);
+                double[] roots = CalculateRoot(quadA, quadB, discriminant);
+                Console.WriteLine("One quadratic root: " + roots[0]);
             }
             else
             {
-                Console.WriteLine("No real quadratic roots exist");
+                double[] roots = CalculateRoot(quadA, quadB, discriminant);
+                Console.WriteLine("Two quadratic roots: " + roots[0] + ", " + roots[1]);
             }
 
         }
 
+        public static double CalculateDiscrimant(double quadA, double quadB, double quadC)
+        {
+            //Discriminant = b^2 - 4ac
+
+            return Math.Pow(quadB, 2) - (4 * quadA * quadC);
+        }
+        public static double[] CalculateRoot(double quadA, double quadB, double discriminant)
+        {
+            //Quadratic Formula
+            //ax^2 + bx + c = 0 
+            //x = (-b +/- Sqrt(b^2 - 4ac)) / 2a
+            double root1 = (-quadB + Math.Sqrt(discriminant)) / (2 * quadA);
+            double root2 = (-quadB - Math.Sqrt(discriminant)) / (2 * quadA);
+            return new double[] { root1, root2 };
+
+        }
 
         public static double ValidDouble(string Output)
         {
