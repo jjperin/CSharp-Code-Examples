541b78889e8b67a812f2e2123494bdc36c57714c
jake peringatt <jakeperingatt@outlook.com>
2026-08-03 18:51:50 +0100

    Added sorts.cs to show sorting algorithm and the efficiencies of bubble and merge

--- CSharp Code Examples/Basics/HelloLincoln.cs
+++ CSharp Code Examples/Basics/HelloLincoln.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CSharp_Code_Examples.Basics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,12 +19,13 @@ namespace CSharp_Code_Examples
             {
                 running = cliMenu();
             }
+
         }
 
         public static bool cliMenu()
         {
             Console.WriteLine("\nC# Examples Menu. Please type the number of the menu you want to open");
-            string[] menuItems = { "Maths", "DataTypes", "Classes", "Inheritance", "Collections", "Excepition Handling", "Exit" };
+            string[] menuItems = { "Maths", "DataTypes", "Sorts", "Classes", "Inheritance", "Collections", "Excepition Handling", "Exit" };
             int index = 1;
             foreach (string item in menuItems)
             {
@@ -37,11 +39,12 @@ namespace CSharp_Code_Examples
             {
                 case "1": Maths.run(); break;
                 case "2": DataTypes.run(); break;
-                case "3": Classes.Classes.run(); break;
-                case "4": Inheritance.Inheritance.run(); break;
-                case "5": Collections.Collections.run(); break;
-                case "6": Exception_Handling.ExceptionHandling.run(); break;
-                case "7": 
+                case "3": Sorts.run(); break;
+                case "4": Classes.Classes.run(); break;
+                case "5": Inheritance.Inheritance.run(); break;
+                case "6": Collections.Collections.run(); break;
+                case "7": Exception_Handling.ExceptionHandling.run(); break;
+                case "8": 
                     Console.WriteLine("Press any key to exit");
                     Console.ReadKey();
                     return false;
---
+++ CSharp Code Examples/Basics/Sorts.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Code_Examples.Basics
+{
+    internal class Sorts
+    {
+        //Authors: Jake Peringatt
+        //A class demonstrating sorts, bubble sort and merge sort, and comparing the time taken
+        public static void run()
+        {
+
+            int[] num1 = new int[20000]; //creating a huge array to show how long it takes for large datasets
+            Random random = new Random();
+
+            for (int i = 0; i < num1.Length; i++)
+            {
+                num1[i] = random.Next(1, 50000); //randomising each array
+            }
+
+            int[] num2 = (int[])num1.Clone();
+
+            Console.WriteLine("Sorting algorithms:");
+
+            Console.WriteLine("Bubble Sort");
+            Console.WriteLine("Tap any key to Sort...");
+            Console.ReadKey();
+            Console.WriteLine();
+
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            BubbleSort(num1);
+            stopwatch.Stop();
+
+            Console.WriteLine("Sorted Array");
+
+            Console.WriteLine("Time elapsed to sort(ticks): " + stopwatch.ElapsedTicks);
+            Console.WriteLine("Time elapsed to sort(ms): " + stopwatch.ElapsedMilliseconds);
+
+
+            Console.WriteLine();
+            Console.WriteLine("Merge Sort");
+            Console.WriteLine("Tap any key to Sort...");
+            Console.ReadKey();
+            Console.WriteLine();
+
+
+
+            stopwatch.Restart();
+            MergeSort(num2, 0, num2.Length - 1);
+            stopwatch.Stop();
+            Console.WriteLine("Sorted Array");
+
+            Console.WriteLine("Time elapsed to sort(ticks): " + stopwatch.ElapsedTicks);
+            Console.WriteLine("Time elapsed to sort(ms): " + stopwatch.ElapsedMilliseconds);
+
+
+
+        }
+
+        public static void BubbleSort(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 0; j < (array.Length - 1) - i; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j+1] = temp;
+                    }
+                }
+            }
+            
+        }
+
+        public static void MergeSort(int[] array, int left, int right)
+        {
+            if (left < right)
+            {
+                int middle = left + (right - left) / 2;
+
+                MergeSort(array, left, middle);
+                MergeSort(array, middle + 1, right);
+
+                Merge(array, left, middle, right);
+            }
+        }
+
+        public static void Merge(int[] array, int left, int middle, int right)
+        {
+            int lSize = middle - left + 1;
+            int rSize = right - middle;
+
+            int[] lArr = new int[lSize];
+            int[] rArr = new int[rSize];
+
+            for (int i = 0; i < lSize; i++)
+            {
+                lArr[i] = array[left + i];
+            }
+            for (int i = 0; i < rSize; i++)
+            {
+                rArr[i] = array[middle + i + 1];
+            }
+
+            int a = 0;
+            int b = 0;
+
+            int k = left;
+            while (a < lSize && b < rSize)
+            {
+                if (lArr[a] <= rArr[b])
+                {
+                    array[k] = lArr[a];
+                    a++;
+                }
+                else
+                {
+                    array[k] = rArr[b];
+                    b++;
+                }
+
+                k++;
+            }
+
+            while (a < lSize)
+            {
+                array[k] = lArr[a];
+                a++;
+                k++;
+
+            }
+            while (b < rSize)
+            {
+                array[k] = rArr[b];
+                b++;
+                k++;
+            }
+
+        }
+    }
+
+}
